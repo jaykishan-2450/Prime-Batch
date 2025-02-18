e82c1c94fc4f58afe8d82e293a1efb46e329d3c9
jaykishan-2450 <122507794+jaykishan-2450@users.noreply.github.com>
2025-02-18 16:15:01 +0530

    m

--- Day3/assignment.js
+++ Day3/assignment.js
@@ -1,36 +1,38 @@
-const user=prompt("Enter your name");
+const user = prompt("Enter your name");
 
-const nos=parseInt(prompt("Enter the number of subjects:"));
+const nos = parseInt(prompt("Enter the number of subjects:"));
 
-let obj={};
+let obj = {};
 console.log(obj);
-let highest=0,sum=0,subj;
-for(let i=0;i<nos;i++){
-    let sub=prompt("Enter the subject");
-    let marks=parseInt(prompt("Enter the marks"));
-    obj[sub]=marks;
-    sum+=marks;
-    if(marks>highest){
-        highest=marks;
-        subj=sub;
+let highest = 0, sum = 0, subj;
+for (let i = 0; i < nos; i++) {
+    let sub = prompt("Enter the subject");
+    let marks = parseInt(prompt("Enter the marks"));
+    obj[sub] = marks;
+    sum += marks;
+    if (marks > highest) {
+        highest = marks;
+        subj = sub;
     }
 }
-console.log("Highest marks::",highest,"in subject",subj);
-let perc=(sum/nos);
-console.log("Percentage::",perc);
+console.log("Highest marks::", highest, "in subject", subj);
+let perc = (sum / nos);
+console.log("Percentage::", perc);
 
 
 
-if(perc>90){
+if (perc > 90) {
     console.log("Grade:: A+");
-}else if(perc>80){
-    console.log("Grade:: A");}
-else if(perc>70){
-    console.log("Grade:: B");}
-else if(perc>60){
+} else if (perc > 80) {
+    console.log("Grade:: A");
+}
+else if (perc > 70) {
+    console.log("Grade:: B");
+}
+else if (perc > 60) {
     console.log("Grade:: C");
-}else if(perc>=33){
+} else if (perc >= 33) {
     console.log("Grade:: D");
-}else{
+} else {
     console.log("Fail");
 }
