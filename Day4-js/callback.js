1949823bbed69ac7f481f630338298e3357ce246
jaykishan-2450 <122507794+jaykishan-2450@users.noreply.github.com>
2025-02-18 16:54:24 +0530

    callbacka and array methods

--- Day4-js/callback.js
+++ Day4-js/callback.js
@@ -39,10 +39,72 @@ const printPretty=(txt)=>{
     console.log(txt);
     console.log("------------");
 }
-sum(20,30,printPretty); 
+// sum(20,30,printPretty); 
 
 
 //sum is called first and then printPretty is called;
 //This (printPretty fn is callback fn) is callback fn example and sum is "higher order fn" as it takes another fn as argument
 
 //---------------------------
+
+const arr=[10,20,30]
+//using foreach
+
+const newArray=[];
+arr.forEach((elem)=>{
+    newArray.push(elem*2);
+})
+// console.log(newArray)
+
+//using map
+const mapExample=arr.map((a)=>{
+    return a*3;
+
+})
+// console.log(mapExample)
+
+//reduce method
+
+//with default
+arr.reduce((acc,elem,idx,array)=>{
+    // console.log(acc,elem,idx,array)
+    return acc //goes to next iteration
+
+},0)
+// if no default are there then first element is taken as default
+
+//with default value
+arr.reduce((acc,elem,idx,array)=>{
+    // console.log(acc,elem,idx,array)
+    return acc //goes to next iteration
+
+})
+//one less iteration as first value of array is taken as deafult acc
+
+//usecase sum
+const ans=arr.reduce((acc,elem,idx,array)=>{
+    console.log(acc,elem,idx,array)
+    return acc+elem //goes to next iteration
+
+},0)
+console.log(ans)//ans is 60
+
+//multiplication
+
+let pro=arr.reduce((acc,elem)=>{
+    // console.log(acc,elem)
+    return acc*elem //goes to next iteration
+
+})
+// console.log(pro)
+
+
+//can reduce be used as map
+
+const newArr=arr.reduce((acc,elem)=>{
+    acc.push(elem*2);
+    return acc;
+},[])
+// console.log(newArr)
+
+//yes
